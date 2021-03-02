b819628f53f16590aa35555a154cc7611c176d1e
mayanze <1229509678@qq.com>
2021-03-02 13:54:53 +0800

    本地错误可以提示到前段，打包发布后提示不到气短问题

--- src/main/java/org/mayanze/dcims/excel/ExcelController.java
+++ src/main/java/org/mayanze/dcims/excel/ExcelController.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Workbook;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -72,7 +73,7 @@ public class ExcelController {
      * @param files
      */
     @SneakyThrows
-    @PostMapping("/tsyw_offter")
+    @PostMapping(value = "/tsyw_offter",produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
     public void tsyw_offter(HttpServletRequest req, HttpServletResponse rep, @RequestParam("file") MultipartFile[] files) {
         Workbook workbook = excelService.tsyw_offter(files);
         _offter_workbook = workbook;
